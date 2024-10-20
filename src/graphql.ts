d97ae647b4da3f187af603de276269dcad3b44cf
artlu99 <alu99.sm@gmail.com>
2024-10-20 19:29:41 -0400

    channel owner permissions management

--- src/graphql.ts
+++ src/graphql.ts
@@ -40,3 +40,105 @@ export const getTextByCastHash = async (
     throw new Error("Failed to get cast by hash");
   }
 };
+
+export const checkIsChannelEnabled = async (
+  channelId: string,
+  env: Bindings
+) => {
+  const graphQLClient = new GraphQLClient(env.YOGA_WHISTLES_ENDPOINT, {
+    headers: { authorization: `Bearer ${env.YOGA_WHISTLES_BEARER}` },
+  });
+
+  try {
+    const enabledChannels = await graphQLClient.request<{
+      getEnabledChannels: string[];
+    }>(
+      gql`
+        query getEnabledChannels {
+          getEnabledChannels
+        }
+      `
+    );
+    return enabledChannels.getEnabledChannels.includes(channelId);
+  } catch (error: any) {
+    if (error.response) {
+      console.error("Error response:", error);
+    }
+    throw new Error("Failed to get channels list");
+  }
+};
+
+export const enableChannel = async (
+  channelId: string,
+  parentUrl: string,
+  env: Bindings
+) => {
+  const graphQLClient = new GraphQLClient(env.YOGA_WHISTLES_ENDPOINT, {
+    headers: { authorization: `Bearer ${env.YOGA_WHISTLES_BEARER}` },
+  });
+
+  try {
+    const enableChannel = await graphQLClient.request<{
+      EnableChannel: {
+        message: string;
+        success: string;
+      };
+    }>(
+      gql`
+        mutation EnableChannel($channelId: String!, $parentUrl: String!) {
+          enableChannel(
+            input: { channelId: $channelId, parentUrl: $parentUrl }
+          ) {
+            message
+            success
+          }
+        }
+      `,
+      {
+        channelId,
+        parentUrl,
+      }
+    );
+    return enableChannel;
+  } catch (error: any) {
+    if (error.response) {
+      console.error("Error response:", error);
+    }
+    throw new Error("Failed to enable channel");
+  }
+};
+
+export const disableChannel = async (channelId: string, env: Bindings) => {
+  const graphQLClient = new GraphQLClient(env.YOGA_WHISTLES_ENDPOINT, {
+    headers: { authorization: `Bearer ${env.YOGA_WHISTLES_BEARER}` },
+  });
+
+  try {
+    const disableChannel = await graphQLClient.request<{
+      disableChannel: {
+        message: string;
+        success: string;
+      };
+    }>(
+      gql`
+        mutation DisableChannel($channelId: String!) {
+          disableChannel(
+            input: { channelId: $channelId }
+          ) {
+            message
+            success
+          }
+        }
+      `,
+      {
+        channelId,
+      }
+    );
+    return disableChannel;
+  } catch (error: any) {
+    if (error.response) {
+      console.error("Error response:", error);
+    }
+    throw new Error("Failed to disable channel");
+  }
+};
